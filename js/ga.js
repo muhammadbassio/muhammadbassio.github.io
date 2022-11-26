ad9be354667cdfc35e28eb3cd7ea29f00771aeb3
Muhammad Bassio <muhammadbassio@gmail.com>
2022-11-26 15:00:57 +0200

    Add playstore link, GA-4

--- js/ga.js
+++ js/ga.js
@@ -1,23 +1,7 @@
 (function () {
-  "use strict";
   window.dataLayer = window.dataLayer || [];
-  function gtag() { dataLayer.push(arguments); }
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
-  gtag('config', 'UA-20158204-6');
-  
-  var trackOutboundLink = function (url) {
-    gtag('event', 'click', {
-      'event_category': 'outbound',
-      'event_label': url,
-      'transport_type': 'beacon',
-      'event_callback': function () { }
-    });
-  };
-  
-  $('a').each(function () {
-    $(this).on("click", function () { 
-      trackOutboundLink($(this).attr('href'));
-    });
-  });
-  
+
+  gtag('config', 'G-TJ98R8M1X7');
 }());
